ff9812252bbb1c532a65fbf330465412d55600d5
stephenwilson <swilson2999@gmail.com>
2018-03-27 13:13:45 -0400

    Added switch statements for OPCODE to CPU.  Includes Add and Subtract

--- src/main/CPU.java
+++ src/main/CPU.java
@@ -6,10 +6,43 @@ public class CPU {
     private int[] registers = new int[16];
 
     public void setRegister(int register, int value){
+
         registers[register] = value;
     }
 
-    public int getRegister(int register){
+    public void step(Memory m){
+        ARMInst inst = ((MemWdInst)m.getMemory(registers[PC])).getInst();
+        registers[PC] += 4; // advance the Process Counter to next instruction
+        System.out.println(inst);
+        int rd = 0, rn = 0, rm = 0, immediate = 0;
+        switch(inst.getOpCode()) {
+            case LDR;
+                rd = inst.getRD();
+                rn = inst.getRN();
+                immediate = inst.getLdrStrImmediate();
+                registers[rd] = ((MemWdData)m.getMemory(registers[rn] +
+                    immediate)).getData();
+                break;
+
+            case ADD:
+                // todo - update so ADD processes second operand correctly,
+                // including shifts and rotates
+
+                rd = inst.getRd();
+                rn = inst.getRn();
+                rm = inst.getRm();
+                registers[rd] = registers[rn] + registers[rm];
+                break;
+            case SDR:
+                rd = inst.getRd();
+                rn = inst.getRn();
+                immediate = inst.getLdrStrImmediate();
+                m.setMemory(registers[rn] + immediate, MemType.Data, ""
+                        + registers[rd]);
+        }
+    }
+    public int getRegister(int register)
+    {
         return registers[register];
     }
 
