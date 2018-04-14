e792a94b71c9ee8e2fbae137c2252de5bf55d7f3
benevolentdarkness <jalexandererickson@gmail.com>
2018-04-13 21:17:25 -0400

    Changed registers to hashtable and modded opcodes

--- src/main/CPU.java
+++ src/main/CPU.java
@@ -1,9 +1,12 @@
 package main;
 
+import java.util.Hashtable;
+
 public class CPU {
 
     public final static int PC = 15;
-    private int[] registers = new int[16];
+    //private int[] registers = new int[16];
+    private final Hashtable<Integer, Integer> registers = new Hashtable<>();
     private static CPU cpu = new CPU();
     //Memory mem = new Memory();
     
@@ -13,15 +16,16 @@ public class CPU {
 
     public void setRegister(int register, int value){
 
-        registers[register] = value;
+        registers.put(register, value);
     }
 
     public void step(CpuMemory m){
-        ARMInst inst = ((MemWdInst)m.getMemory(registers[PC])).getInst();
-        StatFlags instat = StatFlags.instance();
+        
+        ARMInst inst = ((MemWdInst)m.getMemory(registers.get(PC))).getInst();
+        //StatFlags instat = StatFlags.instance();
 
 
-        registers[PC] += 4; // advance the Process Counter to next instruction
+        registers.put(PC, registers.get(PC) + 4); // advance the Process Counter to next instruction
         System.out.println(inst);
         int rd = 0, rn = 0, rm = 0, immediate = 0, cpsr = 0;
         switch(inst.getOpCode()) {
@@ -29,8 +33,9 @@ public class CPU {
                 rd = inst.getRd();
                 rn = inst.getRn();
                 immediate = inst.getLdrStrImmediate();
-                registers[rd] = ((MemWdData)m.getMemory(registers[rn] +
-                    immediate)).getData();
+                registers.put(rd, ((MemWdData)m.getMemory(registers.get(rn) + immediate)).getData());
+                //registers[rd] = ((MemWdData)m.getMemory(registers[rn] +
+                  //  immediate)).getData();
                 break;
 
             case ADD:
@@ -38,10 +43,10 @@ public class CPU {
                 rn = inst.getRn();
                 rm = inst.getRm();
                 if(inst.isImmed()) {
-                    registers[rd] = registers[rn] + inst.getImmedVal();
+                    registers.put(rd, registers.get(rn) + inst.getImmedVal());
                 }
                 else {
-                    registers[rd] = registers[rn] + registers[rm];
+                    registers.put(rd, registers.get(rn) + registers.get(rm));
                 }
                 break;
 
@@ -50,10 +55,10 @@ public class CPU {
                 rn = inst.getRn();
                 rm = inst.getRm();
                 if(inst.isImmed()) {
-                    registers[rd] = registers[rn] - inst.getImmedVal();
+                    registers.put(rd, registers.get(rn) - inst.getImmedVal());
                 }
                 else {
-                    registers[rd] = registers[rn] - registers[rm];
+                    registers.put(rd, registers.get(rn) - registers.get(rm));
                 }
                 break;
 
@@ -63,10 +68,10 @@ public class CPU {
                 rm = inst.getRm();
                 // TODO: Double check this is how immediate is implemented
                 if(inst.isImmed()) {
-                    registers[rd] = inst.getImmedVal() - registers[rn];
+                    registers.put(rd, inst.getImmedVal() - registers.get(rn));
                 }
                 else {
-                    registers[rd] = registers[rm] - registers[rn];
+                    registers.put(rd, registers.get(rm) - registers.get(rn));
                 }
                 break;
 
@@ -74,18 +79,18 @@ public class CPU {
                 rd = inst.getRd();
                 rn = inst.getRn();
                 immediate = inst.getLdrStrImmediate();
-                m.setMemory(registers[rn] + immediate, MemType.DATA, ""
-                        + registers[rd]);
+                m.setMemory(registers.get(rn) + immediate, MemType.DATA, ""
+                        + registers.get(rd));
                 break;
             case AND:
                 rd= inst.getRd();
                 rn = inst.getRn();
                 rm = inst.getRm();
                 if(inst.isImmed()) {
-                    registers[rd] = registers[rn] & inst.getImmedVal();
+                    registers.put(rd, registers.get(rn) & inst.getImmedVal());
                 }
                 else {
-                    registers[rd] = registers[rm] & registers[rn];
+                    registers.put(rd, registers.get(rn) & registers.get(rm));
                 }
                 break;
 
@@ -95,10 +100,10 @@ public class CPU {
                 rm = inst.getRm();
                 //registers[rd] = registers[rn] !& registers[rm];
                 if(inst.isImmed()) {
-                    registers[rd] = ~(registers[rn] & inst.getImmedVal());
+                    registers.put(rd, ~(registers.get(rn) & inst.getImmedVal()));
                 }
                 else {
-                    registers[rd] = ~(registers[rn] & registers[rm]);
+                    registers.put(rd, ~(registers.get(rn) & registers.get(rm)));
                 }
                 break;
             case EOR: //Exclusive Or
@@ -106,10 +111,10 @@ public class CPU {
                 rn = inst.getRn();
                 rm = inst.getRm();
                 if(inst.isImmed()) {
-                    registers[rd] = registers[rn] ^ inst.getImmedVal();
+                    registers.put(rd, registers.get(rn) ^ inst.getImmedVal());
                 }
                 else {
-                    registers[rd] = registers[rn] ^ registers[rm];
+                    registers.put(rd, registers.get(rn) ^ registers.get(rm));
                 }
                 break;
             case ORR: //bitwise Or
@@ -117,12 +122,12 @@ public class CPU {
                 rn = inst.getRn();
                 rm = inst.getRm();
                 if(inst.isImmed()) {
-                    registers[rd] = registers[rn] | inst.getImmedVal();
+                    registers.put(rd, registers.get(rn) | inst.getImmedVal());
                 }
                 else {
-                    registers[rd] = registers[rn] | registers[rm];
+                    registers.put(rd, registers.get(rn) | registers.get(rm));
                 }
-                registers[rd] = registers[rn] | registers[rm];
+                //registers[rd] = registers[rn] | registers[rm];
                 break;
 
 
@@ -135,49 +140,68 @@ public class CPU {
                 rm = inst.getRm();
                 int resultCMN;
                 if(inst.isImmed()){
-                    resultCMN = registers[rn] - inst.getImmedVal();
+                    resultCMN = registers.get(rn) + inst.getImmedVal();
                 }
                 else {
-                    resultCMN = registers[rn] - registers[rm];
+                    resultCMN = registers.get(rn) + registers.get(rm);
                 }
                 if (resultCMN < 0) {
-                    instat.setnegFlag(true);
+                    StatFlags.instance().setnegFlag(true);
                 }
                 else {
-                    instat.setnegFlag(false);
+                    StatFlags.instance().setnegFlag(false);
                 }
                 if (resultCMN == 0) {
-                    instat.setzeroFlag(true);
+                    StatFlags.instance().setzeroFlag(true);
                 }
                 else {
-                    instat.setnegFlag(false);
+                    StatFlags.instance().setzeroFlag(false);
                 }
                 break;
 
-            case CMP: //compare negative
+            case CMP: //compare 
                 //TODO:  Sanity Check this later, add on other flags
 
                 rn = inst.getRn();
-                rm = inst.getRm();
-                int resultCMP;
+                rd = inst.getRd();  
+              //rm = inst.getRm();
+                int resultCMP = 0;
                 if(inst.isImmed()){
-                    resultCMP = registers[rn] + inst.getImmedVal();
+                    resultCMN = registers.get(rd) - inst.getImmedVal();
                 }
                 else {
-                    resultCMP = registers[rn] + registers[rm];
+                    resultCMN = registers.get(rd) - registers.get(rn);
+                }
+                if(inst.isImmed()){
+                    if(registers.get(rd) > inst.getImmedVal()){
+                        StatFlags.instance().setoverflowFlag(true);
+                    }
+                    else{
+                        StatFlags.instance().setoverflowFlag(false);
+                    }    
+                }
+                else{
+                    if(registers.get(rd) > registers.get(rn)){
+                        StatFlags.instance().setoverflowFlag(true);
+                    }
+                    else{
+                        StatFlags.instance().setoverflowFlag(false);
+                    }
+                    
                 }
                 if (resultCMP < 0) {
-                    instat.setnegFlag(true);
+                    StatFlags.instance().setnegFlag(true);
                 }
                 else {
-                    instat.setnegFlag(false);
+                    StatFlags.instance().setnegFlag(false);
                 }
                 if (resultCMP == 0) {
-                    instat.setzeroFlag(true);
+                    StatFlags.instance().setzeroFlag(true);
                 }
                 else {
-                    instat.setnegFlag(false);
+                    StatFlags.instance().setzeroFlag(false);
                 }
+                
                 break;
 
             case MOV:
