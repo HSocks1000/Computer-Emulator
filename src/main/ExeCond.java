56641ac6fa65be4996e0d40a7b59bcab3ddf7066
Sean <sperts3@mail.umw.edu>
2018-04-17 13:46:43 -0400

    Finished the getCondCode status function

--- src/main/ExeCond.java
+++ src/main/ExeCond.java
@@ -2,6 +2,8 @@ package main;
 //Executing Condition Code
 
 
+import static main.CondCode.EQ;
+
 public class ExeCond {
     private boolean condEQ;
     private boolean condNE;
@@ -26,5 +28,89 @@ public class ExeCond {
     }
 
 
-
+    public boolean getCondStatus(CondCode aCondCode) {
+        StatFlags statFlags = StatFlags.instance();
+        boolean zFlag = statFlags.getzeroFlag();
+        boolean nFlag = statFlags.getnegFlag();
+        boolean cFlag = statFlags.getcarryFlag();
+        boolean oFlag = statFlags.getoverflowFlag();
+        switch (aCondCode){
+            case EQ:
+                return zFlag;
+                //break;
+            case NE:
+                return (!zFlag);
+                //break;
+            case CS:
+                return cFlag;
+                //break;
+            case CC:
+                return (!cFlag);
+                //break;
+            case MI:
+                return nFlag;
+                //break;
+            case PL:
+                return (!nFlag);
+                //break;
+            case VS:
+                return oFlag;
+                //break;
+            case VC:
+                return (!oFlag);
+                //break;
+            case HI:
+                if(cFlag && (!zFlag)){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+                //break;
+            case LS:
+                if((!cFlag) || zFlag){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+                //break;
+            case GE:
+                if(nFlag == oFlag){
+                    return true;
+                }
+                else {
+                    return false;
+                }
+                //break;
+            case LT:
+                if(nFlag != oFlag){
+                    return true;
+                }
+                else {
+                    return false;
+                }
+                //break;
+            case GT:
+                if((!zFlag) && (nFlag == oFlag)){
+                    return true;
+                }
+                else {
+                    return false;
+                }
+                //break;
+            case LE:
+                if((zFlag) && (nFlag != oFlag)){
+                    return true;
+                }
+                else {
+                    return false;
+                }
+                //break;
+            case AL:
+                return true;
+                //break;
+        }
+        return false;
+    }
 }
