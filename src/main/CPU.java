223b1cda552a5b5c54085215afa86eaf53dabd0d
Sean <sperts3@mail.umw.edu>
2018-04-20 14:50:31 -0400

    Added in support of branching in CPU class

--- src/main/CPU.java
+++ src/main/CPU.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Map;
+
 public class CPU {
 
     public final static int PC = 15;
@@ -19,238 +21,228 @@ public class CPU {
     public void step(CpuMemory m){
         ARMInst inst = ((MemWdInst)m.getMemory(registers[PC])).getInst();
         StatFlags instat = StatFlags.instance();
-
+        Map<String,MemWd> labels = Memory.instance();
+        ExeCond exeCond = ExeCond.instance();
 
         registers[PC] += 4; // advance the Process Counter to next instruction
         System.out.println(inst);
         int rd = 0, rn = 0, rm = 0, immediate = 0, cpsr = 0;
         long testLong;
-        switch(inst.getOpCode()) {
-            case LDR:
-                rd = inst.getRd();
-                rn = inst.getRn();
-                immediate = inst.getLdrStrImmediate();
-                registers[rd] = ((MemWdData)m.getMemory(registers[rn] +
-                    immediate)).getData();
-                break;
+        if (inst.isBranch()) {
+            if (inst.getCondCode() == CondCode.Empty) {
+                registers[PC] = labels.get(inst.getLabel()).getAddress();
+            }
+            else {
+                if (exeCond.getCondStatus(inst.getCondCode())) {
+                    registers[PC] = labels.get(inst.getLabel()).getAddress();
+                }
+            }
+        }
+        else {
+            switch (inst.getOpCode()) {
+                case LDR:
+                    rd = inst.getRd();
+                    rn = inst.getRn();
+                    immediate = inst.getLdrStrImmediate();
+                    registers[rd] = ((MemWdData) m.getMemory(registers[rn] +
+                            immediate)).getData();
+                    break;
 
-            case ADD:
-                rd = inst.getRd();
-                rn = inst.getRn();
-                rm = inst.getRm();
-                if(inst.isImmed()) {
-                    registers[rd] = registers[rn] + inst.getImmedVal();
-                }
-                else {
-                    registers[rd] = registers[rn] + registers[rm];
-                }
-                break;
+                case ADD:
+                    rd = inst.getRd();
+                    rn = inst.getRn();
+                    rm = inst.getRm();
+                    if (inst.isImmed()) {
+                        registers[rd] = registers[rn] + inst.getImmedVal();
+                    } else {
+                        registers[rd] = registers[rn] + registers[rm];
+                    }
+                    break;
 
-            case SUB: //subtract
-                rd = inst.getRd();
-                rn = inst.getRn();
-                rm = inst.getRm();
-                if(inst.isImmed()) {
-                    registers[rd] = registers[rn] - inst.getImmedVal();
-                }
-                else {
-                    registers[rd] = registers[rn] - registers[rm];
-                }
-                break;
+                case SUB: //subtract
+                    rd = inst.getRd();
+                    rn = inst.getRn();
+                    rm = inst.getRm();
+                    if (inst.isImmed()) {
+                        registers[rd] = registers[rn] - inst.getImmedVal();
+                    } else {
+                        registers[rd] = registers[rn] - registers[rm];
+                    }
+                    break;
 
-            case RSB: //reverse subtract
-                rd = inst.getRd();
-                rn = inst.getRn();
-                rm = inst.getRm();
-                // TODO: Double check this is how immediate is implemented
-                if(inst.isImmed()) {
-                    registers[rd] = inst.getImmedVal() - registers[rn];
-                }
-                else {
-                    registers[rd] = registers[rm] - registers[rn];
-                }
-                break;
+                case RSB: //reverse subtract
+                    rd = inst.getRd();
+                    rn = inst.getRn();
+                    rm = inst.getRm();
+                    // TODO: Double check this is how immediate is implemented
+                    if (inst.isImmed()) {
+                        registers[rd] = inst.getImmedVal() - registers[rn];
+                    } else {
+                        registers[rd] = registers[rm] - registers[rn];
+                    }
+                    break;
 
-            case SDR: //STR?
-                rd = inst.getRd();
-                rn = inst.getRn();
-                immediate = inst.getLdrStrImmediate();
-                m.setMemory(registers[rn] + immediate, MemType.DATA, ""
-                        + registers[rd]);
-                break;
-            case AND:
-                rd= inst.getRd();
-                rn = inst.getRn();
-                rm = inst.getRm();
-                if(inst.isImmed()) {
-                    registers[rd] = registers[rn] & inst.getImmedVal();
-                }
-                else {
-                    registers[rd] = registers[rm] & registers[rn];
-                }
-                break;
+                case SDR: //STR?
+                    rd = inst.getRd();
+                    rn = inst.getRn();
+                    immediate = inst.getLdrStrImmediate();
+                    m.setMemory(registers[rn] + immediate, MemType.DATA, ""
+                            + registers[rd]);
+                    break;
+                case AND:
+                    rd = inst.getRd();
+                    rn = inst.getRn();
+                    rm = inst.getRm();
+                    if (inst.isImmed()) {
+                        registers[rd] = registers[rn] & inst.getImmedVal();
+                    } else {
+                        registers[rd] = registers[rm] & registers[rn];
+                    }
+                    break;
 
-            case BIC: //Bit Clear
-                rd= inst.getRd();
-                rn = inst.getRn();
-                rm = inst.getRm();
-                //registers[rd] = registers[rn] !& registers[rm];
-                if(inst.isImmed()) {
-                    registers[rd] = ~(registers[rn] & inst.getImmedVal());
-                }
-                else {
-                    registers[rd] = ~(registers[rn] & registers[rm]);
-                }
-                break;
-            case EOR: //Exclusive Or
-                rd = inst.getRd();
-                rn = inst.getRn();
-                rm = inst.getRm();
-                if(inst.isImmed()) {
-                    registers[rd] = registers[rn] ^ inst.getImmedVal();
-                }
-                else {
-                    registers[rd] = registers[rn] ^ registers[rm];
-                }
-                break;
-            case ORR: //bitwise Or
-                rd = inst.getRd();
-                rn = inst.getRn();
-                rm = inst.getRm();
-                if(inst.isImmed()) {
-                    registers[rd] = registers[rn] | inst.getImmedVal();
-                }
-                else {
+                case BIC: //Bit Clear
+                    rd = inst.getRd();
+                    rn = inst.getRn();
+                    rm = inst.getRm();
+                    //registers[rd] = registers[rn] !& registers[rm];
+                    if (inst.isImmed()) {
+                        registers[rd] = ~(registers[rn] & inst.getImmedVal());
+                    } else {
+                        registers[rd] = ~(registers[rn] & registers[rm]);
+                    }
+                    break;
+                case EOR: //Exclusive Or
+                    rd = inst.getRd();
+                    rn = inst.getRn();
+                    rm = inst.getRm();
+                    if (inst.isImmed()) {
+                        registers[rd] = registers[rn] ^ inst.getImmedVal();
+                    } else {
+                        registers[rd] = registers[rn] ^ registers[rm];
+                    }
+                    break;
+                case ORR: //bitwise Or
+                    rd = inst.getRd();
+                    rn = inst.getRn();
+                    rm = inst.getRm();
+                    if (inst.isImmed()) {
+                        registers[rd] = registers[rn] | inst.getImmedVal();
+                    } else {
+                        registers[rd] = registers[rn] | registers[rm];
+                    }
                     registers[rd] = registers[rn] | registers[rm];
-                }
-                registers[rd] = registers[rn] | registers[rm];
-                break;
+                    break;
 
 
-             //cpsr based opcode.
+                //cpsr based opcode.
 
-            case CMN: //compare negative
-                //TODO:  Sanity Check this later, add on other flags
+                case CMN: //compare negative
+                    //TODO:  Sanity Check this later, add on other flags
 
-                rn = inst.getRn();
-                rm = inst.getRm();
-                int resultCMN;
-                if(inst.isImmed()){
-                    resultCMN = registers[rn] + inst.getImmedVal();
-                }
-                else {
-                    resultCMN = registers[rn] + registers[rm];
-                }
-                if (resultCMN < 0) {
-                    instat.setnegFlag(true);
-                }
-                else {
-                    instat.setnegFlag(false);
-                }
-                if (resultCMN == 0) {
-                    instat.setzeroFlag(true);
-                }
-                else {
-                    instat.setnegFlag(false);
-                }
-                testLong = registers[rn] + registers[rm];
-                if (resultCMN != testLong){
-                    instat.setcarryFlag(true);
-                }
-                else {
-                    instat.setcarryFlag(false);
-                }
-                if ((registers[rn] > 0) && (registers[rm] > 0)) {
+                    rn = inst.getRn();
+                    rm = inst.getRm();
+                    int resultCMN;
+                    if (inst.isImmed()) {
+                        resultCMN = registers[rn] + inst.getImmedVal();
+                    } else {
+                        resultCMN = registers[rn] + registers[rm];
+                    }
                     if (resultCMN < 0) {
-                        instat.setoverflowFlag(true);
+                        instat.setnegFlag(true);
+                    } else {
+                        instat.setnegFlag(false);
                     }
-                    else {
-                        instat.setoverflowFlag(false);
+                    if (resultCMN == 0) {
+                        instat.setzeroFlag(true);
+                    } else {
+                        instat.setnegFlag(false);
                     }
-                }
-                else if ((registers[rn] < 0) && (registers[rm] < 0)) {
-                    if (resultCMN > 0) {
-                        instat.setoverflowFlag(true);
+                    testLong = registers[rn] + registers[rm];
+                    if (resultCMN != testLong) {
+                        instat.setcarryFlag(true);
+                    } else {
+                        instat.setcarryFlag(false);
                     }
-                    else {
-                        instat.setoverflowFlag(false);
+                    if ((registers[rn] > 0) && (registers[rm] > 0)) {
+                        if (resultCMN < 0) {
+                            instat.setoverflowFlag(true);
+                        } else {
+                            instat.setoverflowFlag(false);
+                        }
+                    } else if ((registers[rn] < 0) && (registers[rm] < 0)) {
+                        if (resultCMN > 0) {
+                            instat.setoverflowFlag(true);
+                        } else {
+                            instat.setoverflowFlag(false);
+                        }
                     }
-                }
-                break;
+                    break;
 
-            case CMP: //compare negative
-                //TODO:  Sanity Check this later, add on other flags
+                case CMP: //compare negative
+                    //TODO:  Sanity Check this later, add on other flags
 
-                rn = inst.getRn();
-                rm = inst.getRm();
-                int resultCMP;
-                if(inst.isImmed()){
-                    resultCMP = registers[rn] - inst.getImmedVal();
-                }
-                else {
-                    resultCMP = registers[rn] - registers[rm];
-                }
-                if (resultCMP < 0) {
-                    instat.setnegFlag(true);
-                }
-                else {
-                    instat.setnegFlag(false);
-                }
-                if (resultCMP == 0) {
-                    instat.setzeroFlag(true);
-                }
-                else {
-                    instat.setnegFlag(false);
-                }
-                testLong = registers[rn] - registers[rm];
-                if (resultCMP != testLong){
-                    instat.setcarryFlag(true);
-                }
-                else {
-                    instat.setcarryFlag(false);
-                }
-                if ((registers[rn] > 0) && (registers[rm] < 0)) {
+                    rn = inst.getRn();
+                    rm = inst.getRm();
+                    int resultCMP;
+                    if (inst.isImmed()) {
+                        resultCMP = registers[rn] - inst.getImmedVal();
+                    } else {
+                        resultCMP = registers[rn] - registers[rm];
+                    }
                     if (resultCMP < 0) {
-                        instat.setoverflowFlag(true);
+                        instat.setnegFlag(true);
+                    } else {
+                        instat.setnegFlag(false);
                     }
-                    else {
-                        instat.setoverflowFlag(false);
+                    if (resultCMP == 0) {
+                        instat.setzeroFlag(true);
+                    } else {
+                        instat.setnegFlag(false);
                     }
-                }
-                else if ((registers[rn] < 0) && (registers[rm] > 0)) {
-                    if (resultCMP > 0) {
-                        instat.setoverflowFlag(true);
+                    testLong = registers[rn] - registers[rm];
+                    if (resultCMP != testLong) {
+                        instat.setcarryFlag(true);
+                    } else {
+                        instat.setcarryFlag(false);
                     }
-                    else {
-                        instat.setoverflowFlag(false);
+                    if ((registers[rn] > 0) && (registers[rm] < 0)) {
+                        if (resultCMP < 0) {
+                            instat.setoverflowFlag(true);
+                        } else {
+                            instat.setoverflowFlag(false);
+                        }
+                    } else if ((registers[rn] < 0) && (registers[rm] > 0)) {
+                        if (resultCMP > 0) {
+                            instat.setoverflowFlag(true);
+                        } else {
+                            instat.setoverflowFlag(false);
+                        }
                     }
-                }
-                break;
+                    break;
 
-            case MOV:
+                case MOV:
 
-                rd = inst.getRd();
-                rm = inst.getRm();
-                if(inst.isImmed()) {
-                    registers[rd] = registers[rm];
-                }
-                else {
-                    registers[rd] = inst.getImmedVal();
-                }
-                break;
+                    rd = inst.getRd();
+                    rm = inst.getRm();
+                    if (inst.isImmed()) {
+                        registers[rd] = registers[rm];
+                    } else {
+                        registers[rd] = inst.getImmedVal();
+                    }
+                    break;
 
-            case MVN:
+                case MVN:
 
-                rd = inst.getRd();
-                rm = inst.getRm();
-                if(inst.isImmed()) {
-                    registers[rd] = ~(registers[rm]);
-                }
-                else {
-                    registers[rd] = ~(inst.getImmedVal());
-                }
-                break;
+                    rd = inst.getRd();
+                    rm = inst.getRm();
+                    if (inst.isImmed()) {
+                        registers[rd] = ~(registers[rm]);
+                    } else {
+                        registers[rd] = ~(inst.getImmedVal());
+                    }
+                    break;
                 //if(resultCMN )
+            }
         }
     }
     public int getRegister(int register)
